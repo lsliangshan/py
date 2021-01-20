891ec8bc3a639e3a02a9d2dbf52b875e78ff8e14
liangshan <liang.shan@zhaopin.com.cn>
2021-01-20 14:29:04 +0800

    test jenkins

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1 style="color: #f33;">Deployed from jenkins</h1>'
+    return '''
+      <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; width: 100%; height: 100%;">
+        <h1 style="color: #f33;">Greeting from jenkins</h1>
+      </div>
+    '''
 
 
 if __name__ == '__main__':
