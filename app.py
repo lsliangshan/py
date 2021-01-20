ca21cc3fbff6ba66371fe33d18e34009ce3c5e61
liangshan <liang.shan@zhaopin.com.cn>
2021-01-20 11:58:43 +0800

    init

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1 style="color: #f33;">Deployed from jenkins</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
