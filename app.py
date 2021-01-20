f13be2e42c88cca8d54b71d0c4c955fcc8331600
liangshan <liang.shan@zhaopin.com.cn>
2021-01-20 14:37:46 +0800

    no message

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''
-      <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; width: 100%; height: 100%;">
-        <h1 style="color: #f33;">Greeting from jenkins</h1>
-      </div>
-    '''
+    # return '''
+    #   <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; width: 100%; height: 100%;">
+    #     <h1 style="color: #f33;">Greeting from jenkins</h1>
+    #   </div>
+    # '''
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    error = None
+    if (request.method == 'POST'):
+        if request.form['username'] != 'ls' or request.form['password'] != '123123':
+            error = '账号或密码错误'
+        else:
+            flash('登录成功')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
